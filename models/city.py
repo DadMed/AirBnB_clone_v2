5aecca26f39eae83ca23784236239ad84e77b497
DadMed <mohammed.dad.ens@gmail.com>
2024-04-14 17:28:33 +0100

    task 2

--- models/city.py
+++ models/city.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import Place
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
--- models/state.py
+++ models/state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
 from models.city import City
-from os import getenv
-from models import storage_type
+import shlex
 
 class State(BaseModel):
     """ State class """
