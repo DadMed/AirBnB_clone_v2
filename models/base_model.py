67ac68ab357f021c7d07f83f3774333330868441
Bettyfm <brieclou@gmail.com>
2024-04-14 09:29:33 -0700

    lets try

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+
 import uuid
 import models
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
-from models import storage
+
 
 Base = declarative_base()
 
@@ -14,8 +15,8 @@ class BaseModel:
     """A base class for all hbnb models"""
 
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
-    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
