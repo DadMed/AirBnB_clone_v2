8f60107e66847f8d2a1d245d3678e70cce892bcc
DadMed <mohammed.dad.ens@gmail.com>
2024-04-17 23:59:51 +0100

    tasks all

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,5 @@
--- To prepare a MySQL server for the project
--- create project developement database with the name:hbnb_dev_db
+-- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- to create new user named : hbnb_dev+all privileges on the db hbnb_dev_db
--- with the password : hbnb_dev_pwd if it dosen't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- to grant all privileges to the new user
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
--- to grant the SELECT privilege for the user hbnb_dev in the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
