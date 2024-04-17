a11c8b8ee4cda484452768abdfa5f3f4ad10fde3
DadMed <mohammed.dad.ens@gmail.com>
2024-04-18 00:31:08 +0100

    tasks all

--- models/city.py
+++ models/city.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """This is the city class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 from models.place import Place
+from models import storage
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """This is the class for City
     Attributes:
         state_id: The state id
@@ -16,6 +15,7 @@ class City(BaseModel, Base):
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="cities")
+
