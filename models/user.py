a601a242501e3e5a48278f8dcbb9ce382be05c0a
Bettyfm <bettyfraigui@gmail.com>
2024-04-17 19:34:48 -0700

    test

--- models/user.py
+++ models/user.py
@@ -1,27 +1,48 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
+"""This module defines a class User"""
+import uuid
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+
+    def __init__(self, *args, **kwargs):
+        """Instantiation of User"""
+        super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        """Returns a string"""
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """Returns a string representation"""
+        return self.__str__()
+
+    def save(self):
+        """Updates the public instance attribute updated_at to current"""
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """Creates dictionary of the class and returns"""
+        my_dict = dict(self.__dict__)
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop('_sa_instance_state', None)
+        return my_dict
+
+    def delete(self):
+        """Delete object"""
+        models.storage.delete(self)
+
