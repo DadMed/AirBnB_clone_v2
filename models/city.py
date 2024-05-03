fcc681d709959b5ec4016b48c7437cc9644e52ae
DadMed <mohammed.dad.ens@gmail.com>
2024-05-03 22:47:20 +0100

    all tasks

--- models/city.py
+++ models/city.py
@@ -1,34 +1,16 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+"""This is the city class"""
 
 
-<<<<<<< HEAD
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
-        state_id = ''
-=======
+
 class City(BaseModel, Base):
-    """This is a class for a city to be related to a state
+    """This is the class for City
     Attributes:
-        state_id: The id of the stated related to the city 
-        name: city name to be input
+        state_id: The state id
+        name: input name
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     places = relationship("Place", cascade='all, delete, delete-orphan',
                           backref="cities")
->>>>>>> b8e5c771846ed8674acc1919da431306245aec87
--- models/user.py
+++ models/user.py
@@ -1,73 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-<<<<<<< HEAD
-
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-=======
-import uuid
-from datetime import datetime
-from sqlalchemy import Column, String, DateTime
-from models.base_model import BaseModel, Base
->>>>>>> b8e5c771846ed8674acc1919da431306245aec87
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-<<<<<<< HEAD
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
-=======
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-
-    def __init__(self, *args, **kwargs):
-        """Instantiation of User"""
-        super().__init__(*args, **kwargs)
-
-    def __str__(self):
-        """Returns a string"""
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
-
-    def __repr__(self):
-        """Returns a string representation"""
-        return self.__str__()
-
-    def save(self):
-        """Updates the public instance attribute updated_at to current"""
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
-
-    def to_dict(self):
-        """Creates dictionary of the class and returns"""
-        my_dict = dict(self.__dict__)
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        my_dict.pop('_sa_instance_state', None)
-        return my_dict
-
-    def delete(self):
-        """Delete object"""
-        models.storage.delete(self)
-
->>>>>>> b8e5c771846ed8674acc1919da431306245aec87
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
